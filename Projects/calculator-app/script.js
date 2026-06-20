1b651641cc738d6e79360aac5359ce2fedc669b5
Vaseem Akram <vaseemakrama366@gmail.com>
2026-06-20 21:10:33 +0530

    Add JavaScript Calculator Project

---
+++ Projects/calculator-app/script.js
@@ -0,0 +1,32 @@
+const display = document.getElementById("display");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const value = button.innerText;
+
+        if(value === "C"){
+            display.value = "";
+        }
+
+        else if(value === "="){
+
+            try{
+                display.value = eval(display.value);
+            }
+
+            catch{
+                display.value = "Error";
+            }
+        }
+
+        else{
+            display.value += value;
+        }
+
+    });
+
+});
